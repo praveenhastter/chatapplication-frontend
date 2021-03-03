ff660e7ca26e2355de4d3725db608610ba685132
praveenhastter <praveen@hasttertech.com>
2021-03-03 18:02:58 +0530

    inital commit

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Chat from "./chat/chat";
+import Process from "./process/process";
+// import "./App.scss";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./home/home";
+import io from "socket.io-client";
+const socket = io("http://localhost:9000/chatapp");
+function Appmain(props) {
+  return (
+    <React.Fragment>
+      <div className="right">
+        <Chat
+            username = {props.match.params.username}
+            roomname = {props.match.params.roomname}
+            socket   = {socket}
+        />
+      </div>
+    </React.Fragment>
+  );
+}
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <Home socket={socket} />
+          </Route>
+          <Route path="/chat/:roomname/:username" component={Appmain} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
